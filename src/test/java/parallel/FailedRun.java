0162320d7140077a8616d0679f8137ff811ebb5a
Dileep <dileepkumarpaidi@gmail.com>
2021-05-19 17:06:04 +0530

    Re Run Failed scenarios

--- src/test/java/parallel/FailedRun.java
+++ src/test/java/parallel/FailedRun.java
@@ -13,7 +13,7 @@ import io.cucumber.testng.CucumberOptions;
 				}, 
 		monochrome = true,
 		glue = { "parallel" },
-		features = { "@target/failedRerun.txt" }
+		features = { "@target/failedRerun.txt" } //Re run path
 )
 public class FailedRun extends AbstractTestNGCucumberTests {
 	@Override
